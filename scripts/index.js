981eb2df5802ed0ad26d64eaed98a66cc82711a4
sgtkotowsk1 <orlov.k.ev@gmail.com>
2024-10-09 18:03:31 +0700

    PR5 is ready

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,56 @@
-// @todo: Темплейт карточки
+const cardList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+const createCard = (card) => {
+    const cardTemplate = document.querySelector('#card-template').content
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const deleteButton = cardElement.querySelector('.card__delete-button');
 
-// @todo: Функция создания карточки
+    cardElement.querySelector('.card__image').setAttribute('src', card.link);
+    cardElement.querySelector('.card__title').textContent = card.name;
 
-// @todo: Функция удаления карточки
+    cardList.append(cardElement);
+    
+    deleteButton.addEventListener('click', () => deleteCard(cardElement));
+}
 
-// @todo: Вывести карточки на страницу
+const deleteCard = (card) => {
+    card.remove();
+}
+
+initialCards.forEach((card) => {
+    createCard(card);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
