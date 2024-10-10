022d8db841542f08097fdc2bf05f65040e061f10
sgtkotowsk1 <orlov.k.ev@gmail.com>
2024-10-10 12:52:02 +0700

    fix remark

--- scripts/index.js
+++ scripts/index.js
@@ -1,56 +1,25 @@
 const cardList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content
 
-const createCard = (card) => {
-    const cardTemplate = document.querySelector('#card-template').content
+const createCard = (card, deleteCard) => {
     const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
     const deleteButton = cardElement.querySelector('.card__delete-button');
+    const cardImageElement = cardElement.querySelector('.card__image');
 
-    cardElement.querySelector('.card__image').setAttribute('src', card.link);
+    cardImageElement.src = card.link
+    cardImageElement.alt = card.name
     cardElement.querySelector('.card__title').textContent = card.name;
 
-    cardList.append(cardElement);
-    
     deleteButton.addEventListener('click', () => deleteCard(cardElement));
+   
+    return cardElement
 }
 
-const deleteCard = (card) => {
+function deleteCard(card) {
     card.remove();
 }
 
 initialCards.forEach((card) => {
-    createCard(card);
+    const cardElement = createCard(card, deleteCard)
+    cardList.append(cardElement);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
