60f428b93bbe5722bc62112475a54f2789ce4686
Орлов Кирилл <orlov.k.ev@gmail.com>
2024-11-29 18:26:04 +0700

    Деструктурирован объект CardData

--- src/modules/cards.js
+++ src/modules/cards.js
@@ -1,38 +1,43 @@
-const createCard = ({
-  name,
-  link,
-  handleRemoveCard,
-  openImagePopup,
-  handleLikeClick,
-  cardTemplate,
-  cardId,
-  cardLike,
-  cardOwner,
-  currentUserId,
-}) => {
+const createCard = (cardData) => {
+  const {
+    name,
+    link,
+    handleRemoveCard,
+    openImagePopup,
+    handleLikeClick,
+    cardTemplate,
+    cardId,
+    cardLike,
+    cardOwner,
+    currentUserId,
+  } = cardData;
+
+  
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
   const deleteButton = cardElement.querySelector(".card__delete-button");
   const likeButton = cardElement.querySelector(".card__like-button");
-  
+
   const likeCountElement = cardElement.querySelector(".card__like-count");
   const cardImageElement = cardElement.querySelector(".card__image");
-  
-  cardElement.setAttribute('card-id', cardId)
+
+  cardElement.setAttribute("card-id", cardId);
   cardImageElement.src = link;
   cardImageElement.alt = name;
   likeCountElement.textContent = cardLike.length;
-  
+
   if (cardLike.find((userLike) => userLike._id === currentUserId)) {
-    likeButton.classList.add("card__like-button_is-active")
+    likeButton.classList.add("card__like-button_is-active");
   }
 
   if (cardOwner._id === currentUserId) {
-    deleteButton.classList.add('card__delete-button-is-active')
+    deleteButton.classList.add("card__delete-button-is-active");
   }
-  
+
   cardElement.querySelector(".card__title").textContent = name;
 
-  deleteButton.addEventListener("click", () => handleRemoveCard(cardId, cardElement));
+  deleteButton.addEventListener("click", () =>
+    handleRemoveCard(cardId, cardElement)
+  );
 
   likeButton.addEventListener("click", (evt) =>
     handleLikeClick(evt, cardId, likeCountElement)
